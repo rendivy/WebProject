c880c91b3d617fcf0043290b2b10ff1ffe23bd1d
Yura <arlosaneq@gmail.com>
2023-04-04 23:29:02 +0700

    Big algorithm and drawer refactoring, new button design

--- clustering/ui/drawAlgorithm.js
+++ clustering/ui/drawAlgorithm.js
@@ -3,48 +3,43 @@ const ctx = canvas.getContext("2d");
 const circleColor = "#000000FF";
 let dots = [];
 
+canvas.addEventListener("mousedown", handleMouseDown);
+window.addEventListener("resize", resizeCanvas);
+
 function resizeCanvas() {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     drawDots();
 }
 
-// вызов resizeCanvas() один раз при загрузке страницы
-resizeCanvas();
-
-// обработчик события resize на объект window
-window.addEventListener("resize", resizeCanvas);
-
-canvas.addEventListener("mousedown", function (e) {
+function handleMouseDown(e) {
     const rect = canvas.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
     dots.push([x, y]);
     drawDots();
-    ctx.beginPath();
-    ctx.arc(x, y, 17, 0, Math.PI * 2);
-    ctx.fillStyle = circleColor;
-    ctx.fill();
-    ctx.stroke();
-});
+    drawCircle(x, y);
+}
 
 function drawDots() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#000000";
     ctx.strokeStyle = "#000000";
-    for (let i = 0; i < dots.length; i++) {
-        ctx.beginPath();
-        ctx.arc(dots[i][0], dots[i][1], 15, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.stroke();
-    }
+    dots.forEach(([x, y]) => {
+        drawCircle(x, y);
+    });
 }
 
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
 }
+
+canvas.clear = function () {
+    ctx.clearRect(0,0, 15000, 15000);
+    dots.length = 0;
+}
+
+resizeCanvas();

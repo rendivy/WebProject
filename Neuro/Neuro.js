6be62439b2f9dce1eb3b18f21608413bab5a184f
Artem Gorst <mmeetteeoorr@mail.ru>
2023-04-04 18:52:00 +0700

    init commit

--- Neuro/Neuro.js
+++
@@ -1,8 +0,0 @@
-let neurons = new Array(10);
-
-for(let i = 0; i < 10; i++) {
-    neurons[i] = new Array(50);
-  for(let j = 0; j < 50; j++) {
-    neurons[i][j] = new Array(50);
-  }
-}
---
+++ Neuro/draw.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+ctx.globalCompositeOperation = 'source-over';
+ctx.globalAlpha = 1;
+ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
+ctx.fillStyle = "#FFFFFF";
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseout", stopDrawing);
+
+
+function startDrawing(e) {
+  isDrawing = true;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+}
+
+function draw(e) {
+  if (!isDrawing) return;
+
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+}
+
+function stopDrawing() {
+  isDrawing = false;
+}
+
